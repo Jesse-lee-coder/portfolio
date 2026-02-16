29d83d71b0a2eb1ebf7798af150139229d959393
jesse-lee-coder <wauer.j@hotmail.de>
2026-02-16 15:31:48 +0100

    feat: implement references section

--- src/app/homepage/homepage.component.ts
+++ src/app/homepage/homepage.component.ts
@@ -3,6 +3,7 @@ import { HeroSectionComponent } from '../hero-section/hero-section.component';
 import { WhyMeSectionComponent } from '../why-me-section/why-me-section.component';
 import { MySkillsSectionComponent } from '../my-skills-section/my-skills-section.component';
 import { MyProjectsSectionComponent } from '../my-projects-section/my-projects-section.component';
+import { ReferencesSectionComponent } from '../references-section/references-section.component';
 
 
 @Component({
@@ -12,7 +13,8 @@ import { MyProjectsSectionComponent } from '../my-projects-section/my-projects-s
     HeroSectionComponent,
     WhyMeSectionComponent,
     MySkillsSectionComponent,
-    MyProjectsSectionComponent
+    MyProjectsSectionComponent,
+    ReferencesSectionComponent
   ],
   templateUrl: './homepage.component.html',
   styleUrl: './homepage.component.scss'
